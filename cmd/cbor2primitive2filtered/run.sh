ef936f42596a85aa11066a5cbb9029d4d09ce182
yanagitani <takanori.yanagitani@gmail.com>
2024-11-14 08:09:19 +0900

    docs: regexp filter example

--- cmd/cbor2primitive2filtered/run.sh
+++ cmd/cbor2primitive2filtered/run.sh
@@ -7,12 +7,15 @@ ufilter=iheight=u:634
 ifilter=id=s:-1
 ffilter=dist=f:42.195
 sfilter=date=S:2024/11/12
+rfilter=name='R:^sky[0-9]'
 
 cat "${ijson}" |
 	json2map2cbor |
 	./cbor2primitive2filtered \
 		"${sfilter}" \
-		"${bfilter}" |
+		"${bfilter}" \
+		"${rfilter}" \
+		|
 	python3 \
 		-m uv \
 		tool \
